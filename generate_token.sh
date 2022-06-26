2e062e4377a916865c7f2153b7d4444746693d8d
Zazer Coner's <96803085+ZazerConer@users.noreply.github.com>
2022-06-27 07:05:19 +0800

    Create generate_token.sh

---
+++ generate_token.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo -e "\e[1;31m [INFO] Create Folder \e[0m"
+mkdir m3u8
+
+echo -e "\e[1;31m [INFO] Downloading Eleven 1 \e[0m"
+Eleven1="https://raw.githubusercontent.com/ZazerConer/dnd/main/grant/elv1/index.m3u8"$AUTH
+wget -O Eleven1.m3u8 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31" $Eleven1
+cp 'Eleven1.m3u8' 'm3u8/Eleven1.m3u8'
+rm 'Eleven1.m3u8'
+
+echo -e "\e[1;31m [INFO] Downloading Eleven 2 \e[0m"
+Eleven2="https://raw.githubusercontent.com/ZazerConer/dnd/main/grant/elv2/index.m3u8"$AUTH
+wget -O Eleven2.m3u8 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31" $Eleven2
+cp 'Eleven2.m3u8' 'm3u8/Eleven2.m3u8'
+rm 'Eleven2.m3u8'
+
+echo -e "\e[1;31m [INFO] Downloading Eleven 3 \e[0m"
+Eleven3="https://raw.githubusercontent.com/ZazerConer/dnd/main/grant/elv3/index.m3u8"$AUTH
+wget -O Eleven3.m3u8 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31" $Eleven3
+cp 'Eleven3.m3u8' 'm3u8/Eleven3.m3u8'
+rm 'Eleven3.m3u8'
+
+echo -e "\e[1;31m [INFO] Downloading Eleven 4 \e[0m"
+Eleven4="https://raw.githubusercontent.com/ZazerConer/dnd/main/grant/elv4/index.m3u8"$AUTH
+wget -O Eleven4.m3u8 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31" $Eleven4
+cp 'Eleven4.m3u8' 'm3u8/Eleven4.m3u8'
+rm 'Eleven4.m3u8'
+
+echo -e "\e[1;31m [INFO] Downloading Eleven 5 \e[0m"
+Eleven5="https://raw.githubusercontent.com/ZazerConer/dnd/main/grant/elv5/index.m3u8"$AUTH
+wget -O Eleven5.m3u8 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31" $Eleven5
+cp 'Eleven5.m3u8' 'm3u8/Eleven5.m3u8'
+rm 'Eleven5.m3u8'
+
+echo -e "\e[1;31m [INFO] Downloading Eleven 6 \e[0m"
+Eleven6="https://raw.githubusercontent.com/ZazerConer/dnd/main/grant/elv6/index.m3u8"$AUTH
+wget -O Eleven6.m3u8 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31" $Eleven6
+cp 'Eleven6.m3u8' 'm3u8/Eleven6.m3u8'
+rm 'Eleven6.m3u8'
+
+echo -e "\e[1;31m [INFO] Generating Date & Time file in UTC+08 \e[0m"
+echo "`env TZ=UTC-08 date`" > "Date & Time UTC +08.txt"
+
+echo -e "\e[1;31m [INFO] Done \e[0m"
